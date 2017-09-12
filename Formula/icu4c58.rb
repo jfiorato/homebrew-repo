216578566d5358a61476024e0356133adc0c3a22
Jim Fiorato <jfiorato@gmail.com>
2017-09-12 14:50:48 -0500

    add icu4c58 formula

---
+++ Formula/icu4c58.rb
@@ -0,0 +1,28 @@
+class Icu4c < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "http://site.icu-project.org/"
+  url "https://ssl.icu-project.org/files/icu4c/58.2/icu4c-58_2-src.tgz"
+  version "58.2"
+  md5 "fac212b32b7ec7ab007a12dff1f3aea1"
+
+  bottle do
+    cellar :any
+  end
+
+  keg_only :provided_by_osx, "macOS provides libicucore.dylib (but nothing else)"
+
+  def install
+    args = %W[--prefix=#{prefix} --disable-samples --disable-tests --enable-static]
+    args << "--with-library-bits=64" if MacOS.prefer_64_bit?
+
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+  end
+end
